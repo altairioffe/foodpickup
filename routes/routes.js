9c9a4405a38ab048b690d3b50c6559e45f0ab0a1
Altair <altair.ioffe@gmail.com>
2020-03-11 16:32:05 +0000

    set up dependencies and middleware in routes.js for twilio

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,21 @@
-const express           = require('express');
-const router            = express.Router();
-const twilio            = require('twilio');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+//Load Env Variables
+require('dotenv').config();
+const TWILIO_ACCOUNT_SID    = process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN         = process.env.TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER       = process.env.TWILIO_PHONE_NUMBER,
+  CUSTOMER_PHONE_NUMBER     = process.env.CUSTOMER_PHONE_NUMBER,
+  RESTAURANT_PHONE_NUMBER   = process.env.RESTAURANT_PHONE_NUMBER;
 
 
+//Middleware
+const express               = require('express');
+const router                = express.Router();
+const twilio                = require('twilio');
+const MessagingResponse     = require('twilio').twiml.MessagingResponse;
+const client                = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+
+//ROUTES:
 router.get("/", (req, res) => {
   res.render("landing_page");
 });
