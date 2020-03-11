26344b3777aad57d212caea968d1131231a5abdd
Altair <altair.ioffe@gmail.com>
2020-03-11 16:18:17 +0000

    added twilio dependency in package.JSON, added twilio route in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router  = express.Router();
+const express           = require('express');
+const router            = express.Router();
+const twilio            = require('twilio');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 
 router.get("/", (req, res) => {
@@ -11,8 +13,26 @@ router.get("/payment", (req, res) => {
 });
 
 router.post("/payment", (req, res) => {
-res.redirect("/payment");
+  res.redirect("/payment");
 });
+
+
+//Twilio route
+//1. CUSTOMER SUBMITS payment, creates POST request to /message, triggers SMS to restaurant
+router.post('/message', function(req, res, next) {
+  // Use the REST client to send a text message
+  client.messages.create({
+    to: RESTAURANT_PHONE_NUMBER,
+    from: TWILIO_PHONE_NUMBER,
+    body: `New order: ${req.body.message}.\nPlease respond with ETA as numerical value in MINUTES.`
+  })
+  .then(function(message) {
+    res.redirect('/confirmation');
+  })
+  .catch(error => res.status(500).send(error));
+});
+
+
 
 router.get("/confirmation", (req, res) => {
   res.render("confirmation");
