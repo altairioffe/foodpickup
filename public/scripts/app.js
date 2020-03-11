ab09cd9aa378035ad48ac9f87b6ddcc187556d77
victorcwyu <victorcwyu@gmail.com>
2020-03-11 15:58:28 +0000

    Cleaned up and comments to code.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+// global variable declarations to be filled in proceeding functions
 let menuItems = {}
 let order = []
 
@@ -11,16 +12,18 @@ $(() => {
   //   }
   // });;
 
+  // template for menu items
   const createMenuItem = function (item) {
-    return `  <article class = "menu" data-id= ${item.id}>
+    return `
+    <article class = "menu" data-id= ${item.id}>
     <div class="item-image">
-    <img class="ui medium circular image" src=${item.image}>
-  </div>
-  <h3>${item.name}</h3>
-  <h4 class="ui dividing header">${item.price}</h4>
-<button class="addCart ui blue button" tabindex="0">Add to cart</button>
-<h4>${item.description}</h4>
-  </article>`
+      <img class="ui medium circular image" src=${item.image}>
+    </div>
+    <h3>${item.name}</h3>
+    <h4 class="ui dividing header">${item.price}</h4>
+    <button class="addCart ui blue button" tabindex="0">Add to cart</button>
+    <h4>${item.description}</h4>
+    </article>`
   }
 
   // renders menu items
@@ -29,6 +32,8 @@ $(() => {
       $("#menu-container").append(createMenuItem(item));
     }
 
+    //check out the brackets!!!!!
+    // renders order summary/calculator form when menu item is added to order, also adds class to addCart button to disable it
     $(".addCart").click(function () {
       $(".calculator.ui.form").css("visibility","visible")
       event.preventDefault();
@@ -36,22 +41,32 @@ $(() => {
       const itemId = $itemContainer.attr("data-id")
       const itemInfo = menuItems[itemId]
       $itemContainer.find(".addCart").addClass("disabled")
+
+      // console logs to determine targetting/isolation of elements
+      // console.log(itemId)
       // console.log(itemInfo)
       // console.log(menuItems[itemId].name);
       // console.log(menuItems[itemId].price);
+      // console.log(menuItems);
+
+      // variable declarations for tax and total calculations
       const addItem = menuItems[itemId].name;
       const addPreTax = parseFloat(menuItems[itemId].price)
       const addTax = (addPreTax * 0.13).toFixed(2);
       const addTotal = (addPreTax * 1.13).toFixed(2);
-      //added <br><br> to the end of append
-      $(".new-item").append($(`<button class="add ui blue button" tabindex="0">+</button> <button class="remove ui red button" tabindex="0">-</button> <span id="counter">1 X ${addItem}</span> <br> <br>`));
+
+      // buttons are added and taxes/total price for the menu item appear in the summary/calculator form
+      $(".new-item").append($(`<button class="add ui blue button ${itemId}" tabindex="0">+</button> <button class="remove ui red button ${itemId}" tabindex="0">-</button> <span id="counter">1 X ${addItem}</span> <br> <br>`));
       $(".pre-tax").text(`Total Before Tax: $${addPreTax}`)
       $(".tax-amount").text(`13% HST: $${addTax}`)
       $(".total-price").text(`Total Amount: $${addTotal}`)
 
-
       //need to put a cap on how many of one item can be ordered
-      // $itemContainer.find(".add").click(function () {
+
+// console log dish ID ==> isolate/target elements... (must add the id or class to all entries in an object or array)
+
+      // adds items to cart (which have already been added), and updates the tax and total price
+      // $itemContainer.find(".add").click(function () { ==> RAF'S CODE...
       $(".add").click(function () {
         event.preventDefault();
         let i = parseInt($("#counter").text());
@@ -63,7 +78,7 @@ $(() => {
         $(".total-price").text("Total Amount: $" + (i * j * 1.13).toFixed(2))
       });
 
-
+      // removes items from cart (which have already been added), and updates the tax and total price
       $(".remove").click(function () {
         event.preventDefault();
         let i = parseInt($("#counter").text());
--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,27 +1,51 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+//check out the brackets!!!!!
+// renders order summary/calculator form when menu item is added to order, also adds class to addCart button to disable it
   $(".addCart").click(function () {
-    $(".addCart").addClass("disabled")
+    $(".calculator.ui.form").css("visibility", "visible")
     event.preventDefault();
-    $(".new-item").append($(`<button class="add ui blue button" tabindex="0">+</button> <span id="counter">1 X BUCKET OF FRIED CHICKEN</span> <button class="remove ui red button" tabindex="0">-</button>`));
-    let preTax = $(".price").text().slice(1)
-    $(".pre-tax").text(`Total Before Tax: $${preTax}`)
-    //need to setTimeout to make these two appear
-    // $(".tax-amount").text("13% HST: $" + (j * i * 0.13).toFixed(2))
-    // $(".total-price").text("Total Amount: $" + (j * i * 1.13).toFixed(2))
+    const $itemContainer = $(this).parent();
+    const itemId = $itemContainer.attr("data-id")
+    const itemInfo = menuItems[itemId]
+    $itemContainer.find(".addCart").addClass("disabled")
+
+    // console logs to determine targetting/isolation of elements
+    // console.log(itemId)
+    // console.log(itemInfo)
+    // console.log(menuItems[itemId].name);
+    // console.log(menuItems[itemId].price);
+    // console.log(menuItems);
+
+    // variable declarations for tax and total calculations
+    const addItem = menuItems[itemId].name;
+    const addPreTax = parseFloat(menuItems[itemId].price)
+    const addTax = (addPreTax * 0.13).toFixed(2);
+    const addTotal = (addPreTax * 1.13).toFixed(2);
+
+    // buttons are added and taxes/total price for the menu item appear in the summary/calculator form
+    $(".new-item").append($(`<button class="add ui blue button ${itemId}" tabindex="0">+</button> <button class="remove ui red button ${itemId}" tabindex="0">-</button> <span id="counter">1 X ${addItem}</span> <br> <br>`));
+    $(".pre-tax").text(`Total Before Tax: $${addPreTax}`)
+    $(".tax-amount").text(`13% HST: $${addTax}`)
+    $(".total-price").text(`Total Amount: $${addTotal}`)
 
     //need to put a cap on how many of one item can be ordered
+
+    // console log dish ID ==> isolate/target elements... (must add the id or class to all entries in an object or array)
+
+    // adds items to cart (which have already been added), and updates the tax and total price
+    // $itemContainer.find(".add").click(function () { ==> RAF'S CODE...
     $(".add").click(function () {
       event.preventDefault();
       let i = parseInt($("#counter").text());
       i++;
-      $("#counter").text(i + "X BUCKET OF FRIED CHICKEN");
-      let j = parseFloat(preTax)
-      $(".pre-tax").text("Total Before Tax: $" + (j * i).toFixed(2))
-      $(".tax-amount").text("13% HST: $" + (j * i * 0.13).toFixed(2))
-      $(".total-price").text("Total Amount: $" + (j * i * 1.13).toFixed(2))
+      $("#counter").text(`${i} X ${addItem}`);
+      let j = addPreTax
+      $(".pre-tax").text("Total Before Tax: $" + (i * j).toFixed(2))
+      $(".tax-amount").text("13% HST: $" + (i * j * 0.13).toFixed(2))
+      $(".total-price").text("Total Amount: $" + (i * j * 1.13).toFixed(2))
     });
 
-
+    // removes items from cart (which have already been added), and updates the tax and total price
     $(".remove").click(function () {
       event.preventDefault();
       let i = parseInt($("#counter").text());
@@ -34,27 +58,20 @@
       } else {
         i--;
       };
-      $("#counter").text(i + " X BUCKET OF FRIED CHICKEN");
-      let j = parseFloat(preTax)
-      $(".pre-tax").text("Total Before Tax: $" + (j * i).toFixed(2))
-      $(".tax-amount").text("13% HST: $" + (j * i * 0.13).toFixed(2))
-      $(".total-price").text("Total Amount: $" + (j * i * 1.13).toFixed(2))
+      $("#counter").text(`${i} X ${addItem}`);
+      let j = addPreTax
+      $(".pre-tax").text("Total Before Tax: $" + (i * j).toFixed(2))
+      $(".tax-amount").text("13% HST: $" + (i * j * 0.13).toFixed(2))
+      $(".total-price").text("Total Amount: $" + (i * j * 1.13).toFixed(2))
     });
   });
+});
 
 
 
 
 
-
-
-// });
-
-
-
-
-
-
+// ==> sort of a template for making modular functions
 // function addOrder(orderName) ->
 //   Orderelement {
 //     <h4 class="ui dividing header">Order Summary</h4>
@@ -63,4 +80,3 @@
 //     <button class="remove ui red button" tabindex="0">-</button>
 //   create outer div
 //   return the outer div
-// }
